d3838ba860d0e7990087ba288d4c3deb2678eba5
Mike Foley <mfoley@mwfoley.com>
2021-04-05 11:21:58 -1000

    Formatted text

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule LogRotate.Mixfile do
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type "mix help deps" for more examples and options
+  #
   defp deps do
     []
   end
